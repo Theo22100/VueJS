644a1cba25848f5bd3f0f5de0bb260bc2e40d12d
Theo22100 <137443754+Theo22100@users.noreply.github.com>
2023-08-07 15:34:44 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,17 +7,29 @@ new Vue({
 
         link: "https://google.com",
 
-        succes: true,
+        // success: true,    A la place d'avoir cette propriété:
+        cls: 'success',
 
-        persons: ['Alex', 'Barnabé', 'Comore', 'Billy']
+        persons: ['Alex', 'Barnabé', 'Comore', 'Billy'],
+
+        style: {background : '#000'}
     },
 
     methods: {
         close: function () {
-            this.message ="Fermé"
 
-            this.succes=false
+            //this.message ="Fermé"
 
+            this.success=false
+
+        },
+
+        style: function () {
+            if (this.success){
+                return {background: '#00FF00'}
+            } else {
+                return {background: '#FF0000'}
+            }
         }
     }
 
